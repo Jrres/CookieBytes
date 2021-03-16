b45909718bf7402c8d888a3ada4f219454758ed4
jrres <jackrollins123@yahoo.com>
2021-03-16 12:42:29 -0400

    layout
    Made nav override elements with z-index property
    added tabs
    search bar(not working)

--- about.js
+++ about.js
@@ -1,5 +1,5 @@
 'use strict'
-const input = document.querySelector("input");
+const input = document.querySelector("input.aboutme");
  input.addEventListener('click', function(){
     var x = document.getElementById("hidden");
     var g = document.getElementById("a");
@@ -12,7 +12,18 @@ const input = document.querySelector("input");
     if(x.className=="expand"){
         x.className="hidden"
         g.className="rev";
-
+        let p = document.getElementsByTagName('p');
+        let myText;
+        
+        for (i = 0; i < p.length; i++) {
+          if (p[i].innerHTML == "*I'm a full time CS student dev new to web design and data structures seeking to improve my skills in all languages") {
+            // console.log(myText, p[0].innerHTML);
+            myText = p[i];
+            break;
+          }
+        }
+        
+        myText.removeAttribute("hidden"); 
     }
     else{
         g.className="anim";
@@ -20,6 +31,5 @@ const input = document.querySelector("input");
 
     }
     });
-
 
 
