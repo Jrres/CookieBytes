230e44c7571748125d410b7fba420ec071b38ebd
jrres <jackrollins123@yahoo.com>
2021-03-27 02:29:14 -0400

    yes
    yes

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
 let canvas = document.getElementById('gameScreen');
 var ctx = canvas.getContext('2d');
 ctx.fillRect(20,20,100,100);
